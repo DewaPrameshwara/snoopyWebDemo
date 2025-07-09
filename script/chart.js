7954402361a76e89735059ef5887445bfc7661dd
DePram <dprameshwara@gmail.com>
2025-07-09 16:25:59 +0800

    demo version done

--- script/chart.js
+++ script/chart.js
@@ -1,11 +1,11 @@
 const ctx = document.getElementById("Chart");
-const elementSPO2 = document.getElementById("data-spo2");
-const elementBPM = document.getElementById("data-bpm");
+const dataSPO2 = document.getElementById("data-spo2");
+const dataBPM = document.getElementById("data-bpm");
 const monitoringBtn = document.getElementById("monitoring-btn");
-const ssButton = document.getElementById("ssChart");
+const demoMonitoringBtn = document.getElementById("demo-monitoring-btn");
 const alertPlaceHolder = document.querySelector(".alert-placeHolder");
+const ssButton = document.getElementById("ssChart");
 let startMonitor = false;
-import { setUpdateDataHandler, setUpWatch } from "../module/firebase/readDataFromFirebase.js";
 let intervalId;
 
 const chart = new Chart(ctx, {
@@ -49,92 +49,61 @@ const chart = new Chart(ctx, {
 });
 
 monitoringBtn.addEventListener("click", () => {
+  showAlert();
+});
+
+demoMonitoringBtn.addEventListener("click", () => {
   if (!startMonitor) {
     startMonitor = true;
-    monitoringBtn.innerHTML = "Stop Monitoring";
-    monitoringBtn.classList.add("btn-danger");
-    monitoringBtn.classList.remove("btn-primary");
-    readData();
+    simulation();
+    demoMonitoringBtn.innerHTML = "Stop Monitoring (Demo)";
+    demoMonitoringBtn.classList.add("btn-danger");
+    demoMonitoringBtn.classList.remove("btn-success");
   } else {
     clearInterval(intervalId);
     startMonitor = false;
-    monitoringBtn.innerHTML = "Start Monitoring";
-    monitoringBtn.classList.remove("btn-danger");
-    monitoringBtn.classList.add("btn-primary");
+    demoMonitoringBtn.innerHTML = "Start Monitoring (Demo)";
+    demoMonitoringBtn.classList.remove("btn-danger");
+    demoMonitoringBtn.classList.add("btn-success");
   }
 });
 
 ssButton.addEventListener("click", () => {
   const link = document.createElement("a");
   link.href = chart.toBase64Image();
-  link.download = `snoopy's chart~${new Date().toISOString().slice(0, 19).replace("T", "~").replace(/:/g, ".")}~.png`;
+  link.download = `snoopy's chart-${new Date().toISOString().slice(0, 19)}.png`;
   link.click();
 });
 
-function readData() {
-  if (startMonitor) {
-    setUpWatch((activated, wifiConnected, fingerDetected) => {
-      console.log("pp");
-      if (activated == 0) {
-        showAlert("Hidupkan alat terlebih dahulu");
-      } else if (wifiConnected == 0) {
-        showAlert("Terdapat masalah koneksi pada alat");
-      } else if (fingerDetected == 0) {
-        showAlert("Sensor tidak dapat mendeteksi, pastikan alat terpasang dengan baik");
-      } else if (activated == 1 && wifiConnected == 1 && fingerDetected == 1) {
-        setUpdateDataHandler((bpm, spo2) => {
-          updateChart(bpm, spo2);
-        });
-      }
-    });
-  }
+// simulasi
+function simulation() {
+  intervalId = setInterval(() => {
+    const time = new Date().toLocaleTimeString();
+    const value_spo2 = Math.floor(Math.random() * 61) + 40;
+    const value_bpm = Math.floor(Math.random() * 61) + 40;
+
+    chart.data.labels.push(time);
+    chart.data.datasets[0].data.push(value_spo2);
+    chart.data.datasets[1].data.push(value_bpm);
+
+    dataSPO2.innerHTML = `${value_spo2} %`;
+    dataBPM.innerHTML = `${value_bpm} bpm`;
+
+    if (chart.data.labels.length > 20) {
+      chart.data.labels.shift();
+      chart.data.datasets[0].data.shift();
+      chart.data.datasets[1].data.shift();
+    }
+
+    chart.update();
+  }, 1000);
 }
 
-function updateChart(bpm, spo2) {
-  const time = new Date().toLocaleTimeString();
-  chart.data.labels.push(time);
-  chart.data.datasets[0].data.push(spo2);
-  chart.data.datasets[1].data.push(bpm);
-
-  elementSPO2.innerHTML = `${spo2} %`;
-  elementBPM.innerHTML = `${bpm} bpm`;
-
-  if (chart.data.labels.length > 20) {
-    chart.data.labels.shift();
-    chart.data.datasets[0].data.shift();
-    chart.data.datasets[1].data.shift();
-  }
-
-  chart.update();
-}
-
-function showAlert(msg) {
+function showAlert() {
   alertPlaceHolder.innerHTML += `            
   <div class="alert alert-warning alert-dismissible" role="alert">
-    <strong>${msg}</strong>
+    <strong>Nyalakan alat SNOOPY terlebih dahulu!</strong>
     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
   </div>
 `;
 }
-// function readData() {
-//   intervalId = setInterval(() => {
-//     const time = new Date().toLocaleTimeString();
-//     // const value_spo2 = Math.floor(Math.random() * 61) + 40;
-//     // const value_bpm = Math.floor(Math.random() * 61) + 40;
-
-//     chart.data.labels.push(time);
-//     chart.data.datasets[0].data.push(value_spo2);
-//     chart.data.datasets[1].data.push(value_bpm);
-
-//     dataSPO2.innerHTML = `${value_spo2} %`;
-//     dataBPM.innerHTML = `${value_bpm} bpm`;
-
-//     if (chart.data.labels.length > 20) {
-//       chart.data.labels.shift();
-//       chart.data.datasets[0].data.shift();
-//       chart.data.datasets[1].data.shift();
-//     }
-
-//     chart.update();
-//   }, 1000);
-// }
--- script/history.js
+++ script/history.js
@@ -1,28 +1,13 @@
 const historyField = document.querySelector("main");
-import { setUpdateHistoryHandler, noDataExists } from "../module/firebase/readHistoryFromFirebase.js";
+const demoBtn = document.querySelectorAll(".demo-btn");
+const noData = document.querySelector(".no-data");
+const data = historyField.querySelectorAll(".card");
 
-setUpdateHistoryHandler((timestap, bpm, spo2) => {
-  addHistory(timestap, bpm, spo2);
+demoBtn.forEach((e) => {
+  e.addEventListener("click", () => {
+    noData.classList.toggle("d-none");
+    data.forEach((e) => {
+      e.classList.toggle("d-none");
+    });
+  });
 });
-
-noDataExists(() => {
-  noData();
-});
-
-function addHistory(timestamp, bpm, spo2) {
-  historyField.innerHTML += `<div class="card mb-3 ps-2" style="width: 18rem;">
-      <div class="card-body">
-        <h5 class="card-title"><i class="fa-solid fa-calendar-days fa-xl me-2"></i>${timestamp.toLocaleDateString("id-ID")}</h5>
-        <p class="card-text"><strong>Apnea Terdeteksi!</strong> <br>pada pukul ${timestamp.toLocaleTimeString("id-ID")}</p>
-        <p class="card-text"><strong>SPO2: ${spo2}% | BPM: ${bpm} bpm</strong></p>
-      </div>
-  </div>`;
-}
-
-function noData() {
-  historyField.innerHTML += `
-          <div class="no-data">
-            <i class="fa-solid fa-thumbs-up fa-2xl mb-4" style="color: rgba(0, 0, 0, 0.3)"></i>
-            <h3>TIDAK ADA RIWAYAT APNEA</h3>
-          </div>`;
-}
